b6af677a1abbe95cde00780b75e1cc9d37e7afb8
thijssnel <thijssnel@snel.com>
2025-10-07 13:46:10 +0200

    jacobian.py can calculate the full transformation matrix, needs to be tested but foundation is made

--- jacobian.py
+++ jacobian.py
@@ -1,5 +1,18 @@
 import numpy as np
+"""
+this class generates the jacobian matrix for a given industrial robot
+provide the class with a dictionary of joints in the following manner
+{
+rank(int) : 'rotation plane (xy, xz, yz, ee), orientation(x,y,z), length (float)'
+}
+rank(int) = the order of the joint in the kinematic chain from the base starting from 0
 
+rotation plane(xy, xz, yz, ee) = the plane in which the joint rotates or translates, use ee for end effector
+
+orientation(x,y,z) = the axis of rotation or translation of the joint, 
+
+length(float) = the length between current joint and the previous joint
+"""
 class jacobian_generator:
     def __init__(self, joints: dict):
         self.joints = joints
@@ -12,10 +25,27 @@ class jacobian_generator:
         orientation = string[1]
         length = float(string[2])
 
-        idx = key - 1  # key 1 -> q[0], key 2 -> q[1], etc.
+        idx = key
         q = self.q[idx]
 
-        if rotation_plane == 'xy':
+        if rotation_plane == 'ee':
+            if orientation == 'z':
+                return np.array([[1, 0, 0, 0],
+                                 [0, 1, 0, 0],
+                                 [0, 0, 1, length],
+                                 [0, 0, 0, 1]])
+            elif orientation == 'y':
+                return np.array([[1, 0, 0, 0],
+                                 [0, 1, 0, length],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]])
+            elif orientation == 'x':
+                return np.array([[1, 0, 0, length],
+                                 [0, 1, 0, 0],
+                                 [0, 0, 1, 0],
+                                 [0, 0, 0, 1]])
+
+        elif rotation_plane == 'xy':
             if orientation == 'z':
                 return np.array([[np.cos(q), -np.sin(q), 0, 0],
                                  [np.sin(q), np.cos(q), 0, 0],
@@ -31,6 +61,7 @@ class jacobian_generator:
                                  [np.sin(q), np.cos(q), 0, 0],
                                  [0, 0, 1, 0],
                                  [0, 0, 0, 1]])
+            
         elif rotation_plane == 'xz':
             if orientation == 'z':
                 return np.array([[np.cos(q), 0, np.sin(q), 0],
@@ -47,6 +78,7 @@ class jacobian_generator:
                                  [0, 1, 0, 0],
                                  [-np.sin(q), 0, np.cos(q), 0],
                                  [0, 0, 0, 1]])
+            
         elif rotation_plane == 'yz':
             if orientation == 'z':
                 return np.array([[1, 0, 0, 0],
@@ -63,3 +95,12 @@ class jacobian_generator:
                                  [0, np.cos(q), -np.sin(q), 0],
                                  [0, np.sin(q), np.cos(q), 0],
                                  [0, 0, 0, 1]])
+            
+    def total_transformation_matrix(self):
+
+        for key, joint in self.joints.items():
+            if key == 0:
+                T = self.transformation_matrix(key, joint)
+            else:
+                T = np.matmul(T, self.transformation_matrix(key, joint))
+        return T
